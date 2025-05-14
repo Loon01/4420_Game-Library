ee961e732699575c85dfe10de2cf1dd42db80517
matthew-wehrmeyer <mwehrmeyer@csub.edu>
2025-05-14 15:36:52 -0700

    Removed unnecessary comments

--- db/insertGameData.php
+++ db/insertGameData.php
@@ -7,27 +7,6 @@ $jsonString = file_get_contents($path);
 $jsonData = json_decode($jsonString, true);
 
 
-//$password = '1234567890';
-//$insStatement = $pdo->query("UPDATE User SET password = $password WHERE name = 'attilary'");
-// $i = 1;
-// $allUsers = $pdo->query("SELECT COUNT(*) FROM User");
-// $res = $allUsers->fetch(PDO::FETCH_ASSOC);
-// $num = $res['COUNT(*)'];
-// echo $num . "\n";
-// while ($i <= $num ) {
-//     $statement = $pdo->query("SELECT * FROM User WHERE uid = $i");
-//     $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-//     //echo "<pre>";
-//     $item = $rows[0];
-//     $id = $item['uid'];
-//     $userName = $item['name'];
-//     $pass = $item['password'];
-//     echo $id . ' : ' . $userName . ' , ' . $pass . "\n";
-
-//     $i++;
-// }
-//echo "</pre>";
-
 $insertGame = $pdo->prepare("INSERT INTO Game (game_id, g_name, description) VALUES (?, ?, ?)");
 $insertGenre = $pdo->prepare("INSERT INTO Genre (game_id, genre) VALUES (?, ?)");
 $insertDeveloper = $pdo->prepare("INSERT INTO Developer (game_id, developer) VALUES (?, ?)");
@@ -70,19 +49,8 @@ foreach($jsonData as $game)
             }
         } 
     }
-    //echo "\n";
-    /*
-    if(empty($game['summary'])) {
-        echo "\n";
-        echo 'No summary';
-    }
-    else {
-        echo "\n";
-        echo $game['summary']; 
-    }
-        */
-    //echo "\n\n";
+    
 }
-//var_dump($jsonData);
+
 ?>
 
